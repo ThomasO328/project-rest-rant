8d752e5273ac32db11eb4b4dc430bf618056e46a
ThomasO328 <100553765+ThomasO328@users.noreply.github.com>
2022-07-20 20:42:59 -0700

    Form data is getting through

---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'Pie In The Sky',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Italian, Pizza',
+    pic: '/images/pizza.jpg'
+}, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: 'http://placekitten.com/250/250'
+}]
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,11 +1,15 @@
 const React = require('react')
 const Def = require('../default')
 
-function index (data) {
+function index(data) {
     let placesFormatted = data.places.map((place) => {
         return (
             <div className='col-sm-6'>
-                <h2>{place.name}</h2>
+                <h2>
+                    <a href={`/places/${place.id}`} >
+                        {place.name}
+                    </a>
+                </h2>
                 <p className='text-center'>
                     {place.cuisines}
                 </p>
@@ -21,7 +25,7 @@ function index (data) {
             <main>
                 <h1>Places to Rant or Rave About</h1>
                 <div className="row">
-                {placesFormatted}
+                    {placesFormatted}
                 </div>
             </main>
         </Def>
